752a308e1442a48527a13e77fe4652b164066a2e
Nikita Kumari <iamnikita004@gmail.com>
2025-06-28 16:27:34 +0530

    Prepare for deployment

--- config/settings.py
+++ config/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-dgp&0e6#ya^!3mhzjej*jp%+drcyj(zvwiwi@&a0xg634u6bl5'
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iamnikitaa.pythonanywhere.com']
 
 
 
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
